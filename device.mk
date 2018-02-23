96aec37d921779e2453dc0978f401775c9892dcc
Sungwoo Park <swpark@nexell.co.kr>
2018-02-23 13:11:42 +0900

    component: Add components needed by carlife
    This patch adds components needed by carlife.
    
    Change-Id: I625758848d3be6514c59c69582ad6d7b17352b07
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- device.mk
+++ device.mk
@@ -260,3 +260,48 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	device/nexell/navi_ref/init_gms_perm.sh:system/bin/init_gms_perm.sh
 endif
+
+# carlife with android phone
+PRODUCT_PACKAGES += \
+	libbdcl \
+	libdiagnose_usb_bdcl \
+	bdcl
+
+# carlife with iphone
+PRODUCT_PACKAGES += \
+	libusb1.0 \
+	libcnary \
+	libplist \
+	libplist++ \
+	plist_cmp \
+	plist_test \
+	plist_util \
+	libusbmuxdcommon \
+	libusbmuxd \
+	iproxy \
+	libcrypto_openssl \
+	libimobilecommon \
+	libmobiledevice \
+	ideviceinfo \
+	idevicename \
+	idevicepair \
+	idevicesyslog \
+	idevice_id \
+	idevicebackup \
+	idevicebackup2 \
+	ideviceimagemounter \
+	idevicescreenshot \
+	ideviceenterrecovery \
+	idevicedate \
+	ideviceprovision \
+	idevicedebugserverproxy \
+	idevicediagnostics \
+	idevicedebug \
+	idevicenotificationproxy \
+	idevicecrashreport \
+	usbmuxd \
+	libzip \
+	ideviceinstaller
+
+PRODUCT_COPY_FILES += \
+	device/nexell/navi_ref/iproxy.sh:system/bin/iproxy.sh
