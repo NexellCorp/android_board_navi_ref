f305580bc9af1ebb8c56e682c3aa6684eed93968
Sungwoo Park <swpark@nexell.co.kr>
2018-07-27 09:59:38 +0900

    build: Make usb boot image compatible with s5p6818
    This patch adds making nsih binary header to fip-loader-usb.img to be
    compatible with s5p6818 so as to use same boot_by_usb.sh script.
    
    Change-Id: Iadb72f5f9fbcec4f070e5e1876e0952e01d6e557
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- build.sh
+++ build.sh
@@ -56,12 +56,21 @@ if [ "${BUILD_ALL}" == "true" ] || [ "${BUILD_SECURE}" == "true" ]; then
 		"-m 0x40200 -b 3 -p ${DEV_PORTNUM} -m 0x1E0200 -b 3 -p ${DEV_PORTNUM} -m 0x60200 -b 3 -p ${DEV_PORTNUM}"
 
 	file_size=35840
-	dd if=${TOP}/device/nexell/secure/loader-emmc.img of=${TOP}/device/nexell/secure/fip-loader-usb.img seek=0 bs=1
+	dd if=${TOP}/device/nexell/secure/loader-emmc.img of=${TOP}/device/nexell/secure/fip-loader-usb.bin seek=0 bs=1
 	let pos=pos+file_size
 	file_size=28672
-	dd if=${TOP}/device/nexell/secure/bl_mon.img of=${TOP}/device/nexell/secure/fip-loader-usb.img seek=${pos} bs=1
+	dd if=${TOP}/device/nexell/secure/bl_mon.img of=${TOP}/device/nexell/secure/fip-loader-usb.bin seek=${pos} bs=1
 	let pos=pos+file_size
-	dd if=${TOP}/device/nexell/secure/bootloader.img of=${TOP}/device/nexell/secure/fip-loader-usb.img seek=${pos} bs=1
+	dd if=${TOP}/device/nexell/secure/bootloader.img of=${TOP}/device/nexell/secure/fip-loader-usb.bin seek=${pos} bs=1
+
+	load_addr="63c00000"
+	start_addr="63c00000"
+
+	python ${TOP}/device/nexell/tools/nsihtxtmod.py ${DEVICE_DIR} ${TOP}/device/nexell/secure/fip-loader-usb.bin $load_addr $start_addr
+	python ${TOP}/device/nexell/tools/nsihbingen.py ${DEVICE_DIR}/nsih-usbdownload.txt ${DEVICE_DIR}/nsih-usbdownload.bin
+
+	cp ${DEVICE_DIR}/nsih-usbdownload.bin ${TOP}/device/nexell/secure/fip-loader-usb.img
+	dd if=${TOP}/device/nexell/secure/fip-loader-usb.bin >> ${TOP}/device/nexell/secure/fip-loader-usb.img
 fi
 
 if [ "${BUILD_ALL}" == "true" ] || [ "${BUILD_KERNEL}" == "true" ]; then
@@ -180,14 +189,6 @@ make_ext4_recovery_image \
 	${OUT_DIR}/ramdisk-recovery.img \
 	67108864 \
 	${RESULT_DIR}
-
-ADDRESS=0x93c00000
-if [ "${MEMSIZE}" == "2GB" ]; then
-	ADDRESS=0x63c00000
-elif [ "${MEMSIZE}" == "1GB" ]; then
-	ADDRESS=0x83c00000
-fi
-gen_boot_usb_script_4418 nxp4330 ${ADDRESS} ${RESULT_DIR}
 
 make_build_info ${RESULT_DIR}
 
