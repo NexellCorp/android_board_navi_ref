ff32cf3244bb7180c4cb94e8afbbb35f5e3817d2
Sungwoo Park <swpark@nexell.co.kr>
2018-10-31 13:23:27 +0900

    component: Remove adding GMS
    This patch removes adding GMS because GMS is not our role.
    
    Change-Id: I38153049e14d070421322cbe9dd3f3dd8233eefb
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- device.mk
+++ device.mk
@@ -255,21 +255,6 @@ SKIP_BOOT_JARS_CHECK := true
 
 $(call inherit-product, frameworks/base/data/fonts/fonts.mk)
 
-# GMS
-#BUILD_GMS = yes
-ifeq ($(strip $(BUILD_GMS)),yes)
-PRODUCT_PACKAGES += \
-	GoogleLoginService \
-	GoogleServicesFramework \
-	GmsCore \
-	Phonesky \
-	com.google.android.maps.xml \
-	com.google.android.maps.jar \
-	Maps
-PRODUCT_COPY_FILES += \
-	device/nexell/navi_ref/init_gms_perm.sh:system/bin/init_gms_perm.sh
-endif
-
 # carlife with android phone
 PRODUCT_PACKAGES += \
 	libbdcl \
