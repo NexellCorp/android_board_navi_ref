ec16cdfdd0a43da39b3fe12a9f9bad79ebeb6e2c
Allan park <allan.park@nexell.co.kr>
2019-06-05 11:20:26 +0900

    dtimg: add task to create dtb.img
    This patch add task to create dtb.img.
    
    Signed-off-by: Allan park <allan.park@nexell.co.kr>
    Change-Id: I5b450e452857b29d2c813ae7ba1cbc236c07d5c8
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/android_board_navi_ref/+/456908
    Reviewed-by: <benjamin@nexell.co.kr>
    Tested-by: <benjamin@nexell.co.kr>
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/android_board_navi_ref/+/458388
    Reviewed-by: Sungwoo Park <swpark@nexell.co.kr>
    Tested-by: Sungwoo Park <swpark@nexell.co.kr>

---
+++ build/tasks/dtimage.mk
@@ -0,0 +1,21 @@
+ifneq ($(filter navi_ref, $(TARGET_DEVICE)),)
+ifneq ($(TARGET_NO_DTIMAGE), true)
+
+MKDTIMG := device/nexell/tools/mkdtimg
+DTB_DIR := device/nexell/kernel/kernel-4.4.x/arch/arm/boot/dts
+DTB_REV00 := $(DTB_DIR)/s5p4418-navi_ref-rev00.dtb
+DTB_REV01 := $(DTB_DIR)/s5p4418-navi_ref-rev01.dtb
+
+$(PRODUCT_OUT)/dtb.img: $(DTB_REV00) $(DTB_REV01)
+	$(MKDTIMG) create $@ \
+	$(DTB_REV00) --id=0 \
+	$(DTB_REV01) --id=1
+
+droidcore: $(PRODUCT_OUT)/dtb.img
+
+
+# Images will be packed into target_files zip.
+INSTALLED_RADIOIMAGE_TARGET += $(PRODUCT_OUT)/dtb.img
+
+endif
+endif
